2704efcd426503b9f06f8645e93720d30bd14e37
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-03-28 17:44:46 -0500

    Update stream_json_file to python3

--- skills_utils/io.py
+++ skills_utils/io.py
@@ -5,7 +5,7 @@ import logging
 def stream_json_file(local_file):
     for i, line in enumerate(local_file):
         try:
-            data = json.loads(line)
+            data = json.loads(line.decode('utf-8'))
             yield data
         except ValueError as e:
             logging.warning("Skipping line %d due to error: %s", i, e)
