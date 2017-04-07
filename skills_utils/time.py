14ee798a33168db1e58eac1f221a414d703f40bd
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-04-07 09:43:06 -0500

    Migrate remaining utilities [Resolves #2]

--- skills_utils/time.py
+++ skills_utils/time.py
@@ -22,9 +22,26 @@ def quarter_to_daterange(quarter):
     )
 
 
-def datetime_to_quarter(dt):
+def datetime_to_year_quarter(dt):
+    """
+    Args:
+        dt: a datetime
+    Returns:
+        tuple of the datetime's year and quarter
+    """
     year = dt.year
     quarter = int(math.ceil(float(dt.month)/3))
+    return (year, quarter)
+
+
+def datetime_to_quarter(dt):
+    """
+    Args:
+        dt: a datetime
+    Returns:
+        the datetime's quarter in string format (2015Q1)
+    """
+    year, quarter = datetime_to_year_quarter(dt)
     return '{}Q{}'.format(year, quarter)
 
 
